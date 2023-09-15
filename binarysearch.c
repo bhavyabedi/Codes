4532c43df86987353c003555b8b8503e6a703237
bhavyabedi <125470205+bhavyabedi@users.noreply.github.com>
2023-09-15 15:11:06 +0530

    First Commit of the code files

---
+++ binarysearch.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int partition(int a[],int low,int high){
+    int pivot=low;
+    int j=high;
+    int i=low;
+    int temp;
+    while(i<j){
+        while(i<j && a[i]<=a[pivot])
+        i++;
+        while(a[j]>a[pivot])
+        j--;
+        if(i<j){
+            temp=a[j];
+            a[j]=a[i];
+            a[i]=temp;
+        }
+        else{
+            temp=a[pivot];
+            a[pivot]=a[j];
+            a[j]=temp;
+        }
+    }
+    return j;
+}
+void quicksort(int a[],int low,int high){
+    int j;
+    if(low<high){
+        j=partition(a,low,high);
+        quicksort(a,low,j-1);
+        quicksort(a,j+1,high);
+    }
+}
+void main(){
+    int i=0;
+    int a[]={1,4,2,3,7,1,4,9};
+    quicksort(a,0,7);
+    for(i=0;i<8;i++)
+    printf("%3d",a[i]);
+    return 0;
+}
---
+++ first.py
@@ -0,0 +1,2 @@
+print("Hello")
+input(int:"a")
